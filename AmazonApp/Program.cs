ca6029813c711fa3da731a7d141abdebd4317a23
mohamd1542 <mohamdsaloum667@gmail.com>
2023-04-05 11:56:30 +0300

    first commit

---
+++ AmazonApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using AmazonLibrary;
+namespace AmazonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCard = fillTheCart();
+            Customer customer = new Customer();
+            customer.FirstName = "Mohamed";
+            customer.City = "Dubai";
+
+            foreach (var item in myCard)
+            {
+                item.ShipeProduct(customer);
+            }
+        }
+        private static List<Product> fillTheCart()
+        {
+            var card = new List<Product>
+            {
+                new Product { Name = "How to trian your dragon " },
+                new Product { Name = "learn Csharp in 30 min " },
+                new Product { Name = "learn Sql in 30 min " }
+            };
+
+            return card;
+        }
+    }
+}
---
+++ AmazonLibrary/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonLibrary
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public bool IsOrderCompleted { get; set; }
+
+        public void ShipeProduct(Customer customer)
+        {
+            if (this.IsOrderCompleted == false)
+            {
+                Console.WriteLine("Shipping the " + this.Name + " to " + customer.FirstName + " in " + customer.City);
+            }
+        }
+    }
+}
